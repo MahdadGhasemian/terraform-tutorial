1801e3b4be635228a34b494d886e2be44b3178ad
Mahdad Ghasemian <mahdad.ghasemian@gmail.com>
2023-12-16 13:13:22 +0330

    module and count

--- variables.tf
+++ variables.tf
@@ -10,28 +10,13 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "ami_id" {
-  type    = string
-  default = "ami-0230bd60aa48260c6"
+variable "env_name" {
+  type = list
+  default = [
+    "build",
+    "dev",
+    "stage",
+    "production"
+  ]
 }
 
-variable "instance_type" {
-  type    = string
-  default = "t2.micro"
-}
-
-variable "key_name" {
-  type    = string
-  default = "terraform"
-}
-
-variable "tags" {
-  type = object({
-    Name       = string
-    Created_by = string
-  })
-  default = {
-    Name       = "server1"
-    Created_by = "terraform"
-  }
-}
